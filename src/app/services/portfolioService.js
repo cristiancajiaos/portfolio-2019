fd0d8ee7ece18cc830ffaa2aafa9af2ef87c66be
Cristian Cajiao <cristian.cajiao@alumnos.usm.cl>
2020-01-21 17:58:31 -0300

    Carrusel pasado a la sección de portafolio

--- src/app/services/portfolioService.js
+++ src/app/services/portfolioService.js
@@ -11,6 +11,21 @@
         portfolioObj.getPortfolio = function(){
             return $http.get('data/portfolioData.json');
         };
+
+        portfolioObj.getRandomPortfolioItem = function(){
+            var foo;
+
+            $http.get('/data/portfolioData.json').then(function(response){
+                console.log('response');
+                foo = response;
+                console.log(foo);
+            }, function(error){
+                console.log('error');
+            }).finally(function(){
+                console.log('finally');
+                return foo;
+            });
+        };
         
         return portfolioObj;
     }
